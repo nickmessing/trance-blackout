cfcf6a5c137262efcc6ff3b072de304c6452c658
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2018-05-14 23:28:30 +0300

    adsense

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+document.body.innerHTML += `<script src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-6805429465998678",
+    enable_page_level_ads: true
+  });
+</script>`;
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
