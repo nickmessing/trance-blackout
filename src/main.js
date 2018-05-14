bac36b5df607f80ca048df2faabd0127b6a6b388
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2018-05-14 23:38:46 +0300

    analytics

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
-document.body.innerHTML += `<script src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+document.body.innerHTML += `<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119246242-1"></script>
 <script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-6805429465998678",
-    enable_page_level_ads: true
-  });
-</script>`;
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-119246242-1');
+</script>
+`;
 
 new Vue({
   render: h => h(App)
