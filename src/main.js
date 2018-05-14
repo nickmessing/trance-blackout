409dcaf25b8c1e597ba0faa35443dd83a4a72aa0
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2018-05-14 23:41:27 +0300

    fix analytics

--- src/main.js
+++ src/main.js
@@ -2,16 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
-document.body.innerHTML += `<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119246242-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-119246242-1');
-</script>
-`;
 
 new Vue({
   render: h => h(App)
